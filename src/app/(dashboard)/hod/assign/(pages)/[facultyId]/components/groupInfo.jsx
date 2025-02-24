0f4d4c452f975606665e563ab4a4e32768f33db0
vrajpatel01 <vrajp933@gmail.com>
2025-02-24 13:24:25 +0530

    fix hod group not found issue

--- src/app/(dashboard)/hod/assign/(pages)/[facultyId]/components/groupInfo.jsx
+++ src/app/(dashboard)/hod/assign/(pages)/[facultyId]/components/groupInfo.jsx
@@ -1,19 +1,27 @@
-import GroupInfoItem from "./groupInfoItem"
-import { useGetFacultyGroups } from "../../../services/query"
-import Skeleton from "react-loading-skeleton"
+import GroupInfoItem from "./groupInfoItem";
+import { useGetFacultyGroups } from "../../../services/query";
+import Skeleton from "react-loading-skeleton";
 export default function GroupInfo({ facultyId }) {
-    const groups = useGetFacultyGroups(facultyId)
+  const groups = useGetFacultyGroups(facultyId);
 
-    return (
-        <div className="w-full h-full">
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-5">
-                {groups.isPending && Array(10).fill(0).map((_, i) => (
-                    <Skeleton height={100} />
-                ))}
-                {groups.isSuccess && groups?.data?.data.map((group) => (
-                    <GroupInfoItem group={group} deassignBtn={true} link={`/hod/groups/${group?._id}`} />
-                ))}
-            </div>
-        </div>
-    )
+  return (
+    <div className="w-full h-full">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-5">
+        {groups.isPending &&
+          Array(10)
+            .fill(0)
+            .map((_, i) => <Skeleton height={100} />)}
+        {groups.isSuccess &&
+          groups?.data &&
+          groups?.data?.success &&
+          groups?.data?.data.map((group) => (
+            <GroupInfoItem
+              group={group}
+              deassignBtn={true}
+              link={`/hod/groups/${group?._id}`}
+            />
+          ))}
+      </div>
+    </div>
+  );
 }
