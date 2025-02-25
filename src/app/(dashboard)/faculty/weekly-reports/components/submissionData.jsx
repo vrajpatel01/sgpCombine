c715915680098f2ba3edb301e1ba40a639206a02
vrajpatel01 <vrajp933@gmail.com>
2025-02-25 13:40:59 +0530

    fix faculty dashboard overflow issue

--- src/app/(dashboard)/faculty/weekly-reports/components/submissionData.jsx
+++ src/app/(dashboard)/faculty/weekly-reports/components/submissionData.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
@@ -14,176 +14,239 @@ import { useChangeReportSubmissionStatus } from "../services/mutation";
 import toast from "react-hot-toast";
 import { useQueryClient } from "@tanstack/react-query";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
-
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export default function SubmissionData() {
-    const [isLoading, setIsLoading] = useState({
-        accept: false,
-        previews: false
-    })
-    const pathname = usePathname()
-    const week = pathname.split('/')[3]
-    const groupId = pathname.split('/')[4]
-    const queryClient = useQueryClient();
+  const [isLoading, setIsLoading] = useState({
+    accept: false,
+    previews: false,
+  });
+  const pathname = usePathname();
+  const week = pathname.split("/")[3];
+  const groupId = pathname.split("/")[4];
+  const queryClient = useQueryClient();
 
-    const groupData = useGetGroupSubmissionData({ week, groupId })
-    const changeStatus = useChangeReportSubmissionStatus();
-    const [rejectModel, setRejectModel] = useState(false)
+  const groupData = useGetGroupSubmissionData({ week, groupId });
+  const changeStatus = useChangeReportSubmissionStatus();
+  const [rejectModel, setRejectModel] = useState(false);
 
-    if (groupData.isPending) {
-        return (
-            <div className="h-screen w-full flex justify-center items-center">
-                <div className="border-black border-l-2 rounded-full w-14 h-14 animate-spin">
-                </div>
-            </div>
-        )
-    }
-    const url = pathname.split('/').filter((path, index) => index !== 4).join('/')
-    const statusChange = async (isLocked, allowPreviousEdit = false) => {
-        const splitPath = pathname.split('/')
-        const groupId = splitPath.pop();
-        const week = splitPath[splitPath.length - 1]
-        const data = {
-            "isLocked": isLocked,
-            "rejectMessage": "hello",
-            "allowPreviousEdit": allowPreviousEdit
-        }
-
-        changeStatus.mutate({ groupId, week, data }, {
-            onSuccess: async (data) => {
-                if (data.success) {
-                    await queryClient.invalidateQueries(['groupSubmissionData', week, groupId]);
-                    return toast.success(data?.message ?? 'Status changed successfully');
-                }
-                return toast.error(data.message);
-            },
-            onError: (error) => {
-                return toast.error('Something went wrong');
-            },
-            onSettled: () => {
-                setIsLoading({
-                    accept: false,
-                    previews: false
-                })
-            }
-        })
-    }
-    const currentWeek = pathname.split('/')[3];
-    if (!groupData.data || groupData.isError || groupData.data?.data === null) {
-        return (
-            <div className="w-full h-full flex justify-center items-center flex-col">
-                <div className="flex justify-between sm:justify-start items-center gap-3 w-full">
-                    <div className="flex justify-between items-center">
-                        <Link href={url} className="p-2 rounded-full hover:bg-border text-center cursor-pointer">
-                            <span><IoChevronBackSharp size={18} /></span>
-                        </Link>
-                        <h1 className="text-title-28 whitespace-nowrap flex flex-nowrap">Week {currentWeek}</h1>
-                    </div>
-                </div>
-                <div className="bg-white shadow-sm rounded-md p-4 flex justify-center items-center text-center sm:text-left flex-col sm:flex-row gap-3 mt-10">
-                    <BiError className="text-3xl sm:text-2xl" />
-                    Group has not submitted the report for week {week}
-                </div>
-            </div>
-        )
-    }
+  if (groupData.isPending) {
     return (
-        <div className="w-full space-y-5">
-            <div className="header flex justify-between items-center flex-col sm:flex-row w-full gap-4">
-                <div className="flex justify-between sm:justify-start items-center gap-3 w-full">
-                    <div className="flex justify-between items-center">
-                        <Link href={url} className="p-2 rounded-full hover:bg-border text-center cursor-pointer">
-                            <span><IoChevronBackSharp size={18} /></span>
-                        </Link>
-                        <h1 className="text-title-28 whitespace-nowrap flex flex-nowrap">Week {currentWeek} <span className="hidden sm:block"> - {new Date(groupData.data?.data?.updatedAt).toDateString()}</span></h1>
-                    </div>
-                    <span className="capitalize border-border border-1 select-none px-3 py-2 rounded-md">{groupData.data?.data?.status}</span>
-                </div>
-                {groupData.data.data.isLocked && !(groupData.data?.data?.status === 'approved') &&
-                    <div className="flex justify-end items-center gap-4 w-full">
-                        {/* <span className="capitalize border-border border-1 px-3 py-2 rounded-md select-none sm:hidden">{groupData.data?.data?.status}</span> */}
-                        <div className="flex justify-center items-center gap-4">
-                            {currentWeek != "1" &&
-                                <Button onClick={() => {
-                                    setIsLoading({ ...isLoading, previews: true })
-                                    return statusChange(false, true);
-                                }}
-                                    isLoading={isLoading.previews}
-                                    disabled={isLoading.previews}>
-                                    Previews allow
-                                </Button>}
-                            <Dialog>
-                                <DialogTrigger asChild>
-                                    <Button>
-                                        Accept
-                                    </Button>
-                                </DialogTrigger>
-                                <DialogContent>
-                                    <DialogHeader>
-                                        <DialogTitle>Accept the report</DialogTitle>
-                                        <DialogDescription>Are you sure you want to accept this weekly report. after accepting this report you can not able to do any changes.</DialogDescription>
-                                        <div className="flex justify-end">
-                                            <Button
-                                                type="button"
-                                                onClick={() => {
-                                                    setIsLoading({ ...isLoading, accept: true })
-                                                    return statusChange(true);
-                                                }}
-                                                isLoading={isLoading.accept}
-                                                disabled={isLoading.accept}>
-                                                Accept
-                                            </Button>
-                                        </div>
-                                    </DialogHeader>
-                                </DialogContent>
-                            </Dialog>
-                            <Button variant="destructive" onClick={() => setRejectModel(true)}>
-                                Reject
-                            </Button>
-                        </div>
-                    </div>}
-            </div>
+      <div className="h-screen w-full flex justify-center items-center">
+        <div className="border-black border-l-2 rounded-full w-14 h-14 animate-spin"></div>
+      </div>
+    );
+  }
+  const url = pathname
+    .split("/")
+    .filter((path, index) => index !== 4)
+    .join("/");
+  const statusChange = async (isLocked, allowPreviousEdit = false) => {
+    const splitPath = pathname.split("/");
+    const groupId = splitPath.pop();
+    const week = splitPath[splitPath.length - 1];
+    const data = {
+      isLocked: isLocked,
+      rejectMessage: "hello",
+      allowPreviousEdit: allowPreviousEdit,
+    };
 
-            <Tabs defaultValue="overall">
-                <TabsList>
-                    <TabsTrigger value="overall">Overall work</TabsTrigger>
-                    <TabsTrigger value="members">Members work</TabsTrigger>
-                    <TabsTrigger disabled={groupData?.data?.data?.rejectMessages.length === 0} value="rejection">Rejection</TabsTrigger>
-                </TabsList>
-                <TabsContent value="overall">
-                    <form className="mt-5 w-full flex flex-col gap-4">
-                        <WorkDataViewer
-                            name="Expected Outcome as per Timeline"
-                            workDone={groupData.data?.data?.expectedOutcome} />
-                        <WorkDataViewer
-                            name="Work Done"
-                            workDone={groupData.data?.data?.workDone} />
-                    </form>
-                </TabsContent>
-                <TabsContent value="members">
-                    <form className="mt-5 w-full space-y-5">
-                        {groupData.data?.data?.studentsWork.map((studentWork, index) => {
-                            return <WorkDataViewer
-                                key={index}
-                                name={studentWork.name}
-                                email={studentWork.email}
-                                workDone={studentWork.work} />
-                        })}
-                    </form>
-                </TabsContent>
-                <TabsContent value="rejection" className="space-y-5">
-                    {groupData.data?.data?.rejectMessages.map((message, index) => (
-                        <div key={index} className="bg-white p-4 rounded-md border-[.5px] border-border flex flex-col gap-4">
-                            <div>{message.msg}</div>
-                            <div className="text-gray-500 text-sm">{new Date(message.date).toDateString()}</div>
-                        </div>
-                    ))}
-                </TabsContent>
-            </Tabs>
-            <Dialog open={rejectModel} onOpenChange={setRejectModel}>
-                <ReportRejectModel model={rejectModel} setModel={setRejectModel} />
-            </Dialog>
+    changeStatus.mutate(
+      { groupId, week, data },
+      {
+        onSuccess: async (data) => {
+          if (data.success) {
+            await queryClient.invalidateQueries([
+              "groupSubmissionData",
+              week,
+              groupId,
+            ]);
+            return toast.success(
+              data?.message ?? "Status changed successfully"
+            );
+          }
+          return toast.error(data.message);
+        },
+        onError: (error) => {
+          return toast.error("Something went wrong");
+        },
+        onSettled: () => {
+          setIsLoading({
+            accept: false,
+            previews: false,
+          });
+        },
+      }
+    );
+  };
+  const currentWeek = pathname.split("/")[3];
+  if (!groupData.data || groupData.isError || groupData.data?.data === null) {
+    return (
+      <div className="w-full h-full flex justify-center items-center flex-col">
+        <div className="flex justify-between sm:justify-start items-center gap-3 w-full">
+          <div className="flex justify-between items-center">
+            <Link
+              href={url}
+              className="p-2 rounded-full hover:bg-border text-center cursor-pointer"
+            >
+              <span>
+                <IoChevronBackSharp size={18} />
+              </span>
+            </Link>
+            <h1 className="text-title-28 whitespace-nowrap flex flex-nowrap">
+              Week {currentWeek}
+            </h1>
+          </div>
         </div>
-    )
+        <div className="bg-white shadow-sm rounded-md p-4 flex justify-center items-center text-center sm:text-left flex-col sm:flex-row gap-3 mt-10">
+          <BiError className="text-3xl sm:text-2xl" />
+          Group has not submitted the report for week {week}
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="w-full space-y-5">
+      <div className="header flex justify-between items-center flex-col sm:flex-row w-full gap-4">
+        <div className="flex justify-between sm:justify-start items-center gap-3 w-full">
+          <div className="flex justify-between items-center">
+            <Link
+              href={url}
+              className="p-2 rounded-full hover:bg-border text-center cursor-pointer"
+            >
+              <span>
+                <IoChevronBackSharp size={18} />
+              </span>
+            </Link>
+            <h1 className="text-title-28 whitespace-nowrap flex flex-nowrap">
+              Week {currentWeek}{" "}
+              <span className="hidden sm:block">
+                {" "}
+                - {new Date(groupData.data?.data?.updatedAt).toDateString()}
+              </span>
+            </h1>
+          </div>
+          <span className="capitalize border-border border-1 select-none px-3 py-2 rounded-md">
+            {groupData.data?.data?.status}
+          </span>
+        </div>
+        {groupData.data.data.isLocked &&
+          !(groupData.data?.data?.status === "approved") && (
+            <div className="flex justify-end items-center gap-4 w-full">
+              {/* <span className="capitalize border-border border-1 px-3 py-2 rounded-md select-none sm:hidden">{groupData.data?.data?.status}</span> */}
+              <div className="flex justify-center items-center gap-4">
+                {currentWeek != "1" && (
+                  <Button
+                    onClick={() => {
+                      setIsLoading({ ...isLoading, previews: true });
+                      return statusChange(false, true);
+                    }}
+                    isLoading={isLoading.previews}
+                    disabled={isLoading.previews}
+                  >
+                    Allow Previews
+                  </Button>
+                )}
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button>Accept</Button>
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>Accept the report</DialogTitle>
+                      <DialogDescription>
+                        Are you sure you want to accept this weekly report.
+                        after accepting this report you can not able to do any
+                        changes.
+                      </DialogDescription>
+                      <div className="flex justify-end">
+                        <Button
+                          type="button"
+                          onClick={() => {
+                            setIsLoading({ ...isLoading, accept: true });
+                            return statusChange(true);
+                          }}
+                          isLoading={isLoading.accept}
+                          disabled={isLoading.accept}
+                        >
+                          Accept
+                        </Button>
+                      </div>
+                    </DialogHeader>
+                  </DialogContent>
+                </Dialog>
+                <Button
+                  variant="destructive"
+                  onClick={() => setRejectModel(true)}
+                >
+                  Reject
+                </Button>
+              </div>
+            </div>
+          )}
+      </div>
+
+      <Tabs defaultValue="overall">
+        <TabsList>
+          <TabsTrigger value="overall">Overall work</TabsTrigger>
+          <TabsTrigger value="members">Members work</TabsTrigger>
+          <TabsTrigger
+            disabled={groupData?.data?.data?.rejectMessages.length === 0}
+            value="rejection"
+          >
+            Rejection
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="overall">
+          <form className="mt-5 w-full flex flex-col gap-4">
+            <WorkDataViewer
+              name="Expected Outcome as per Timeline"
+              workDone={groupData.data?.data?.expectedOutcome}
+            />
+            <WorkDataViewer
+              name="Work Done"
+              workDone={groupData.data?.data?.workDone}
+            />
+          </form>
+        </TabsContent>
+        <TabsContent value="members">
+          <form className="mt-5 w-full space-y-5">
+            {groupData.data?.data?.studentsWork.map((studentWork, index) => {
+              return (
+                <WorkDataViewer
+                  key={index}
+                  name={studentWork.name}
+                  email={studentWork.email}
+                  workDone={studentWork.work}
+                />
+              );
+            })}
+          </form>
+        </TabsContent>
+        <TabsContent value="rejection" className="space-y-5">
+          {groupData.data?.data?.rejectMessages.map((message, index) => (
+            <div
+              key={index}
+              className="bg-white p-4 rounded-md border-[.5px] border-border flex flex-col gap-4"
+            >
+              <div>{message.msg}</div>
+              <div className="text-gray-500 text-sm">
+                {new Date(message.date).toDateString()}
+              </div>
+            </div>
+          ))}
+        </TabsContent>
+      </Tabs>
+      <Dialog open={rejectModel} onOpenChange={setRejectModel}>
+        <ReportRejectModel model={rejectModel} setModel={setRejectModel} />
+      </Dialog>
+    </div>
+  );
 }
--- src/app/(dashboard)/faculty/weekly-reports/layout.jsx
+++ src/app/(dashboard)/faculty/weekly-reports/layout.jsx
@@ -1,18 +1,20 @@
-'use client'
+"use client";
 import { usePathname } from "next/navigation";
 import { WeekList } from "./components/Weeklist";
 
 export default function SubmissionLayout({ children }) {
-    const pathname = usePathname();
-    return (
-        <div className="submissionSidebar flex justify-start items-start flex-col lg:flex-row lg:w-auto h-auto lg:h-full gap-5">
-            {/* <div className="absolute top-20 left-0 right-0 lg:left-auto md:right-auto lg:bottom-0 lg:top-0 p-5 lg:px-0 z-50"> */}
-            <div className="absolute left-0 right-0 lg:left-auto lg:right-auto lg:bottom-0 lg:top-0 lg:py-5 z-20 min-w-[280px]">
-                <WeekList />
-            </div>
-            <div className="w-full lg:ml-[18.4rem] mt-24 lg:mt-0">
-                {children}
-            </div>
+  const pathname = usePathname();
+  return (
+    <div className="submissionSidebar flex justify-start items-start flex-col lg:flex-row lg:w-auto h-auto lg:h-full gap-5">
+      {/* <div className="absolute left-0 right-0 lg:left-auto lg:right-auto lg:bottom-0 lg:top-0 lg:py-5 z-20 min-w-[280px]"> */}
+      <div className="absolute left-0 right-0 lg:left-auto lg:right-auto lg:bottom-0 lg:top-0 lg:py-5 z-20 min-w-[280px]">
+        <WeekList />
+      </div>
+      <div className="mt-14 lg:mt-0 lg:absolute right-0 top-0 left-0 lg:left-[36rem] bottom-0 no-scrollbar w-full lg:w-auto">
+        <div className="overflow-y-scroll h-full pt-5 lg:pr-5 w-full lg:w-auto p-5 overflow-x-hidden">
+          {children}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
